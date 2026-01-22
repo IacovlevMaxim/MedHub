7e9d2ab49f51749db4d9be191be671f70114622a
CrisLP <cristianlupasco2004@gmail.com>
2026-01-22 17:20:15 +0200

    removed the (tabs) line + bugfix

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout() {
                 onNotificationPress={handleNotificationPress}
               />
             ) : (
-              <Stack screenOptions={{ headerShown: true }}>
+              <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="(tabs)" />
                 <Stack.Screen name="login" />
                 <Stack.Screen name="register" />
